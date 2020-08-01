3a345017c1aabe344f6755cbb8e25472231285b4
Glitch (digislothbot-alpha) <none>
2020-08-01 20:52:54 +0000

    Added code to !poke the Digital Sloth

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ let listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
+
+// Set up files to link up ? with the .env file
+const TARGETCHANNEL = process.env.TARGETCHANNEL;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+
+// Use these values when working locally 
+// const TARGETCHANNEL = process.env.TESTTargetChannel;
+// const USERNAME = process.env.TESTUsername;
+// const PASSWORD = process.env.TESTPassword;
+
 // Setting options for our bot, disable debug output once your up and running.
 let options = {
   options: {
@@ -22,10 +33,10 @@ let options = {
     reconnect: true
   },
   identity: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD
+    username: USERNAME,
+    password: PASSWORD
   },
-  channels: [ process.env.TARGETCHANNEL ]
+  channels: [ TARGETCHANNEL ]
 };
 
 // Set up our new TMI client and connect to the server.
@@ -41,34 +52,34 @@ client.on("connected", (address, port) => {
 // This simple bot will simply monitor chat logging for instances of '!twitter' or '!github'.
 //
 client.on("chat", (channel, user, message, self) => {
-  switch (message) {
+  switch (message.toLowerCase()) {
     case "!twitter":
       client.action(
-        process.env.TARGETCHANNEL,
+        TARGETCHANNEL,
         `${user["display-name"]} you can find it at twitter.com/aaronkirk`
       );
       break;
     case "!github":
       client.action(
-        process.env.TARGETCHANNEL,
+        TARGETCHANNEL,
         `${user["display-name"]} you can find it at github.com/aaronkirk`
       );
       break;
     case "!sloth":
-      client.action(process.env.TARGETCHANNEL, `I am a DigitalSloth`);
+      client.action(TARGETCHANNEL, `I am a DigitalSloth`);
       break;
     case "!ident":
-      client.action(process.env.TARGETCHANNEL, `I am the Digital Sloth, here to serve`);
+      client.action(TARGETCHANNEL, `I am the Digital Sloth, here to serve`);
      break;
     case "!gimmenum":
       var output = getRandInt(500, 550);
-      client.action(process.env.TARGETCHANNEL, `Here is a num ${output}`);
+      client.action(TARGETCHANNEL, `Here is a num ${output}`);
       break;
     case "!listcar":
-      client.action(process.env.TARGETCHANNEL, `The current list of cars is ${carArray.toString()}`);
+      client.action(TARGETCHANNEL, `The current list of cars is ${carArray.toString()}`);
       break;
     case "!log":
-      client.action(process.env.TARGETCHANNEL, `Log Command Posted on Twitch`);
+      client.action(TARGETCHANNEL, `Log Command Posted on Twitch`);
       console.log(`###################### - Log Command Posted in Console`);
       break;
     default:
@@ -77,9 +88,12 @@ client.on("chat", (channel, user, message, self) => {
 });
 
 client.on("chat", (channel, user, message, self) => {
-  if(message === "!a"){
-      client.action(process.env.TARGETCHANNEL, `Great Sucess`);
+  if(message.toLowerCase() === "!a"){
+      client.action(TARGETCHANNEL, `Great Sucess`);
   }
+  // if(user.mod){
+  //   client.action(TARGETCHANNEL, `mod detected`);
+  // }
 });
 
 function getRandInt(min, max) {
@@ -91,4 +105,38 @@ var carArray = new Array("Saab", "Volvo", "BMW");
 function addCar(car) {
   //carArray[carArray.Lenght] = car;
   return carArray.toString();
+}
+
+
+
+client.on("chat", (channel, user, message, self) => {
+  if(message.toLowerCase() === "!poke"){
+      console.log(`"!poke" command recieved -- "${message.toUpperCase()}"`)
+      const digiSlothReply = pokeSloth()
+      client.action(TARGETCHANNEL, digiSlothReply);
+  }
+});
+
+
+function pokeSloth(){
+  var digiSlothReply = "????";
+  const diceRoll = rollDice()
+  const modifier = rollDice()
+  if(diceRoll <= 1){
+    digiSlothReply = `Zzz ${modifier}`
+  } else if (diceRoll > 1 && diceRoll <= 2){
+    digiSlothReply = `Zzzzzzzzzz ${modifier}`
+  } else if (diceRoll > 2 && diceRoll < 3){
+    digiSlothReply = `Zzz Zzz Zzz ${modifier}`
+  } else if (diceRoll == 4){
+    digiSlothReply = `Huh?! I'm Awake ${modifier}`
+  } 
+  console.log(`The DigiSloth was poked and said "${digiSlothReply}" ${modifier}`)
+  return digiSlothReply;
+}
+
+function rollDice(){
+  const diceRoll = Math.floor(Math.random() * 5) + 1;
+  console.log(`The dice has been rolled and returned the number: ${diceRoll}`);
+  return diceRoll;
 }
